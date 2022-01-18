5a9d646df210eb9d490091fb4ccba4d5ad96f7e4
Rada Berar <ujagaga@gmail.com>
2022-01-18 16:25:48 +0100

    Update

--- ArduinoExamples/heater/DS18B20.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_DS18B20_H_
-#define MY_DS18B20_H_
-
-void DS_init(void);
-float DS_read(void);
-
-#endif 
--- ArduinoExamples/heater/wifi_connection.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFI_CONNECTION_H_
-#define WIFI_CONNECTION_H_
-
-#include "config.h"
-
-void WIFIC_connect(void);
-bool WIFIC_connected(void);
-
-#endif 
